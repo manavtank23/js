738cf65ae85f099cf288f449f245c18a5d49e825
manavtank23 <manavtank120@gmail.com>
2026-07-03 20:16:47 +0000

    datatype summary

---
+++ 01_basics/datatype.js
@@ -0,0 +1,14 @@
+// primitive
+//7 types:string,Number,Boolean,BigInt,null,undefined,symbol
+
+//Reference
+// Array,Function,Object
+const heros=["blackpanther","batman"];
+let myobj={
+name:"xoxo",
+age:21,
+}
+
+const myfunc=function(){
+    console.log("hello world");
+}
