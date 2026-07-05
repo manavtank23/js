034864ba2a497a227a432a40f64a4c297ef3cb9a
manavtank23 <manavtank120@gmail.com>
2026-07-05 13:25:47 +0000

    math_and_num

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -12,3 +12,23 @@ age:21,
 const myfunc=function(){
     console.log("hello world");
 }
+
+//************************************** 
+
+//stack(primitive),Heap(non primitive)
+
+let mychannel="mrbeast"
+let anotherchannel=mychannel
+anotherchannel="ishowspeed"
+console.log(mychannel);
+console.log(anotherchannel);
+
+let userone={
+    email:"abc@gmail",
+    upi:123
+}
+let usertwo=userone
+
+usertwo.email="aa@gmail"
+console.log(userone.email);
+console.log(usertwo.email);
