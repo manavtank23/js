28c2daaab0e082756a927682524e7a465757c98a
manavtank23 <manavtank120@gmail.com>
2026-07-05 19:04:57 +0000

    date and time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,23 @@
+//Dates
+
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+
+let myCreatedDate=new Date(2023,0,23,5,3)
+//onsole.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp= Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// //in seconds
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
