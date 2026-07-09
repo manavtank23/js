d86471c8ff052f269c702605fa23b7600c25a594
manavtank23 <manavtank120@gmail.com>
2026-07-09 10:19:03 +0000

    array_pt1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,35 @@
+//array
+
+const Arr=[1,2,3,4]
+const myheros=["blackpanther","superman"]
+
+const myArr=new Array(0,1,2,3,4,5);
+//console.log(myArr);
+
+
+//array methods
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(0)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr=myArr.join()
+// console.log(newArr);
+
+//splice,slice
+
+// console.log("A ",myArr);
+
+ const myn1=myArr.slice(1,3)
+
+//  console.log(myn1);
+//  console.log("B ",myArr);
+
+ const myn2=myArr.splice(1,3)
+//  console.log("c ",myArr);
+//  console.log(myn2);
